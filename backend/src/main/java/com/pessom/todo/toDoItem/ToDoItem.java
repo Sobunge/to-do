5dc400cc8423d47642d9b2ca593655e4a2b01b84
Samuel Odhiambo <samuelobunge@gmail.com>
2023-12-21 18:56:27 +0300

    adding GeneratedValue annoatation to todo id

--- backend/src/main/java/com/pessom/todo/toDoItem/ToDoItem.java
+++ backend/src/main/java/com/pessom/todo/toDoItem/ToDoItem.java
@@ -3,6 +3,8 @@ package com.pessom.todo.toDoItem;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +19,7 @@ import lombok.NoArgsConstructor;
 public class ToDoItem {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     @Enumerated(EnumType.STRING)

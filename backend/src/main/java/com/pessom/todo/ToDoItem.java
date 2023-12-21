54ed3c64bb0c5191847c9ae665d2c16b857d8989
Samuel Odhiambo <samuelobunge@gmail.com>
2023-12-21 13:49:18 +0300

    Adding database configuration and todoitem class

---
+++ backend/src/main/java/com/pessom/todo/ToDoItem.java
@@ -0,0 +1,20 @@
+package com.pessom.todo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ToDoItem {
+    
+    @Id
+    private long id;
+    private String name;
+}

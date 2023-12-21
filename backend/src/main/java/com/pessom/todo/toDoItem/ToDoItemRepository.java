84b998f3cf9ad2155aa95bf55d5a1a8fcb65068f
Samuel Odhiambo <samuelobunge@gmail.com>
2023-12-21 17:42:03 +0300

    updating todoitem service

--- backend/src/main/java/com/pessom/todo/toDoItem/ToDoItemRepository.java
+++ backend/src/main/java/com/pessom/todo/toDoItem/ToDoItemRepository.java
@@ -1,7 +1,11 @@
 package com.pessom.todo.toDoItem;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ToDoItemRepository extends JpaRepository<ToDoItem, Long>{
+
+    List<ToDoItem> findByStatus(Status finished);
     
 }
--- backend/src/main/java/com/pessom/todo/toDoItem/ToDoItemService.java
+++ backend/src/main/java/com/pessom/todo/toDoItem/ToDoItemService.java
@@ -1,5 +1,7 @@
 package com.pessom.todo.toDoItem;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +11,38 @@ public class ToDoItemService {
     @Autowired ToDoItemRepository toDoItemRepository;
 
     //Adding a todo item
+    public ToDoItem addToDoItem(ToDoItem item){
+        return toDoItemRepository.save(item);
+    }
 
     //Updating a todo item
+    public ToDoItem updateToDoItem(ToDoItem updaToDoItem){
+        return toDoItemRepository.save(updaToDoItem);
+    }
 
     //Deleting a todo item
+    public void deleteToDoItem(Long toDoItemId){
+        toDoItemRepository.deleteById(toDoItemId);
+    }
 
     //Getting all todo items
+    public List<ToDoItem> getAllToDoItems(){
+        return toDoItemRepository.findAll();
+    }
 
     //Getting one todo item
+    public ToDoItem getToDoItemById(Long toDoItemId){
+        return toDoItemRepository.findById(toDoItemId).get();
+    }
+
+    //Getting all finished todo items
+    public List<ToDoItem> getAllFinishedToDoItems(){
+        return toDoItemRepository.findByStatus(Status.FINISHED);
+    }
+
+    //Getting all unfinished todo items
+    public List<ToDoItem> getAllUnfinishedToDoItems(){
+        return toDoItemRepository.findByStatus(Status.UNFINISHED);
+    }
 
 }

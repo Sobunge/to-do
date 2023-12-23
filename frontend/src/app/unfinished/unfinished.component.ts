b97d6b3668d8c571bcc9376ee154f90da462dd6c
Samuel Odhiambo <samuelobunge@gmail.com>
2023-12-23 19:42:58 +0300

    adding container class to item list

--- frontend/src/app/unfinished/unfinished.component.ts
+++ frontend/src/app/unfinished/unfinished.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from "../components/task-list/task-list.component";
 
 @Component({
-  selector: 'app-unfinished',
-  standalone: true,
-  imports: [],
-  templateUrl: './unfinished.component.html',
-  styleUrl: './unfinished.component.css'
+    selector: 'app-unfinished',
+    standalone: true,
+    templateUrl: './unfinished.component.html',
+    styleUrl: './unfinished.component.css',
+    imports: [TaskListComponent]
 })
 export class UnfinishedComponent {
 

a196f507acb4904db839c827da57e9b1c09a8e69
Samuel Odhiambo <samuelobunge@gmail.com>
2023-12-24 20:10:25 +0300

    toggle checkboxes

--- frontend/src/app/components/task-list/task-list.component.ts
+++ frontend/src/app/components/task-list/task-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { NavbarComponent } from "../navbar/navbar.component";
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Task } from '../../task';
 import { TaskService } from '../../task.service';
 
@@ -21,6 +21,16 @@ export class TaskListComponent {
 
     ngOnInit() {
         this.tasks = this._taskService.getTasks();
+    }
+
+    handleCheckBoxClick(id: number) {
+        id = id - 1;
+        if (this.tasks[id].status == "finished") {
+            this.tasks[id].status = "unfinished"
+        } else {
+            this.tasks[id].status = "finished"
+        }
+
     }
 
 }

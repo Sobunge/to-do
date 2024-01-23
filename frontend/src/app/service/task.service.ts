02993d2ae43c4ec594c8f1de9f33530e1b24436e
Samuel Odhiambo <samuelobunge@gmail.com>
2024-01-24 01:21:46 +0300

    adding error handling

--- frontend/src/app/service/task.service.ts
+++ frontend/src/app/service/task.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of, tap } from 'rxjs';
+import { Observable, catchError, of, tap, throwError } from 'rxjs';
 import { Task } from '../modal/task';
 import { MessageService } from './message.service';
 import { Message } from '../modal/message';
@@ -27,71 +27,78 @@ export class TaskService {
   getTasks(): Observable<Task[]> {
     this.url = this.taskUrl + "items";
     return this.http.get<Task[]>(this.url)
-    .pipe(
-      catchError(this.handleError<Task[]>("getTasks", []))
-    );
+      .pipe(
+        catchError(this.handleError)
+      );
   }
- 
+
 
   getTask(taskId: number): Observable<Task> {
     this.url = this.taskUrl + "items/" + taskId;
-    return this.http.get<Task>(this.url);
+    return this.http.get<Task>(this.url)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   getFinishedTasks(): Observable<Task[]> {
     this.url = this.taskUrl + "items/finished";
-    return this.http.get<Task[]>(this.url);
+    return this.http.get<Task[]>(this.url)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   getUnfinishedTasks(): Observable<Task[]> {
     this.url = this.taskUrl + "items/unfinished";
     return this.http.get<Task[]>(this.url)
-    .pipe(
-      tap(_ => console.log("Getting tasks successfull")),
-      catchError(this.handleError<Task[]>("getTasks", []))
-    );
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   changeTaskToFinished(taskId: number): Observable<Task> {
     const url = this.taskUrl + "items/" + taskId + "/finished";
-    return this.http.put<Task>(url, {}, this.httpOptions);
+    return this.http.put<Task>(url, {}, this.httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   changeTaskToUnfinished(taskId: number): Observable<Task> {
     const url = this.taskUrl + "items/" + taskId + "/unfinished";
-    return this.http.put<Task>(url, {}, this.httpOptions);
+    return this.http.put<Task>(url, {}, this.httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   addTask(task: Task): Observable<Task> {
     const url = this.taskUrl + "items";
-    return this.http.post<Task>(url, task, this.httpOptions);
+    return this.http.post<Task>(url, task, this.httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   deleteTask(taskId: number): Observable<Task> {
     const url = this.taskUrl + "items/" + taskId;
-    return this.http.delete<Task>(url, this.httpOptions);
+    return this.http.delete<Task>(url, this.httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   *
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.status === 404) {
+      console.error('Resource not found. Handle the error here.');
+      // You can perform additional actions or show a user-friendly message.
+    } else {
+      console.error('An error occurred:', error.error.message || error.statusText);
+    }
 
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      /*this.message = new Message(`${operation} failed: ${error.message}`, "danger");
-      this.messageService.triggerToast(this.message);*/
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+    // Return an observable with a detailed error message
+    return throwError(() => `Error: ${error.status} - ${error.statusText || 'Unknown error'}`);
   }
 
 }

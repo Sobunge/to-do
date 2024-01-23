f52ccf74db5bad72edf101de80427f12926da56e
Samuel Odhiambo <samuelobunge@gmail.com>
2024-01-23 19:30:26 +0300

    cleaning files, removing unused imports

--- frontend/src/app/components/message/message.component.ts
+++ frontend/src/app/components/message/message.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { MessageService } from '../../service/message.service';
 import { Subscription } from 'rxjs';
 import { Message } from '../../modal/message';
@@ -13,20 +13,20 @@ declare var bootstrap: any;
   templateUrl: './message.component.html',
   styleUrl: './message.component.css'
 })
-export class MessageComponent implements OnDestroy{
+export class MessageComponent implements OnDestroy {
 
   @ViewChild('toastLiveExample') toastLiveExample!: ElementRef;
 
   private subscription: Subscription;
   public message: Message = new Message();
 
-  constructor(public messageService: MessageService){
+  constructor(public messageService: MessageService) {
     this.subscription = this.messageService.showToast$.subscribe((message) => {
       this.message = message;
       this.showNotificationToast();
     });
   }
-  
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }

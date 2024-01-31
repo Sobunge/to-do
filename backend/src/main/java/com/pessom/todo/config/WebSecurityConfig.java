8f038a117787c55dca08f66c6f7e38ea09603376
Samuel Odhiambo <samuelobunge@gmail.com>
2024-02-01 00:16:53 +0300

    creating a bean password encoder

--- backend/src/main/java/com/pessom/todo/config/WebSecurityConfig.java
+++ backend/src/main/java/com/pessom/todo/config/WebSecurityConfig.java
@@ -1,5 +1,18 @@
 package com.pessom.todo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean(name = "passwordEncoder")
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+   
     
 }

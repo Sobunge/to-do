803ccce2201d3eaff5f1002c3520e7b86bb78eef
Samuel Odhiambo <samuelobunge@gmail.com>
2024-02-01 12:01:00 +0300

    adding non null annotation in jwtfilter class

--- backend/src/main/java/com/pessom/todo/jwtutils/JwtFilter.java
+++ backend/src/main/java/com/pessom/todo/jwtutils/JwtFilter.java
@@ -1,5 +1,65 @@
 package com.pessom.todo.jwtutils;
 
-public class JwtFilter {
-    
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
+    @Autowired
+    private TokenManager tokenManager;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String tokenHeader = request.getHeader("Authorization");
+        String username = null;
+        String token = null;
+
+        if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+            token = tokenHeader.substring(7);
+
+            try {
+                username = tokenManager.getUsernameFromToken(token);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unable to get JWS token.");
+            } catch (ExpiredJwtException e) {
+                System.out.println("JWS token has expired");
+            }
+
+        } else {
+            System.out.println("Bearer string not found in token.");
+        }
+
+        if (null != username && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (tokenManager.validateJwtToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
 }

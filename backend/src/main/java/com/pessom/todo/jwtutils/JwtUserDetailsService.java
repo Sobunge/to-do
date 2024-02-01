6b1896cbd930e41c3f6073416fa6a8aea0da182f
Samuel Odhiambo <samuelobunge@gmail.com>
2024-02-01 10:56:44 +0300

    adding comment password

--- backend/src/main/java/com/pessom/todo/jwtutils/JwtUserDetailsService.java
+++ backend/src/main/java/com/pessom/todo/jwtutils/JwtUserDetailsService.java
@@ -10,6 +10,7 @@ public class JwtUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Password is password
         if ("sobunge".equals(username)) {
             return new User("sobunge", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
                     new ArrayList<>());

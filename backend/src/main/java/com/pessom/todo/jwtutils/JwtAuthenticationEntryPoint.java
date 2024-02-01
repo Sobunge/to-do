b2f4ae1d00e14fda1eefd6d6bb8f2e0d60a69a93
Samuel Odhiambo <samuelobunge@gmail.com>
2024-02-01 19:02:48 +0300

    adding component annotation in jwtauthentication entry point

--- backend/src/main/java/com/pessom/todo/jwtutils/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/pessom/todo/jwtutils/JwtAuthenticationEntryPoint.java
@@ -5,11 +5,13 @@ import java.io.Serializable;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
 
     @Override

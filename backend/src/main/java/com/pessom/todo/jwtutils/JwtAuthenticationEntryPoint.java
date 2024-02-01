3e5ce35564343c594ee3867644c53fa3c4a79fbf
Samuel Odhiambo <samuelobunge@gmail.com>
2024-02-01 12:05:06 +0300

    popullating jwtAuthenticationEntryPoint

--- backend/src/main/java/com/pessom/todo/jwtutils/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/pessom/todo/jwtutils/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,21 @@
 package com.pessom.todo.jwtutils;
 
-public class JwtAuthenticationEntryPoint {
-    
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+
 }

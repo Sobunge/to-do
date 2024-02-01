d0a50e9d36393fa557d21ebd177e630912daf872
Samuel Odhiambo <samuelobunge@gmail.com>
2024-02-01 11:00:42 +0300

    populating JwtRequestModel

--- backend/src/main/java/com/pessom/todo/jwtutils/model/JwtRequestModel.java
+++ backend/src/main/java/com/pessom/todo/jwtutils/model/JwtRequestModel.java
@@ -1,5 +1,18 @@
 package com.pessom.todo.jwtutils.model;
 
-public class JwtRequestModel {
-    
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class JwtRequestModel implements Serializable {
+
+    private static final long serialVersionUID = 2636936156391265891L;
+    private String username;
+    private String password;
+
 }

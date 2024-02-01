96f271ad5b4b1a6ddc77cd53d18fd2a3f3fdf326
Samuel Odhiambo <samuelobunge@gmail.com>
2024-02-01 10:56:06 +0300

    populating the jwtUserDetailsService

--- backend/src/main/java/com/pessom/todo/jwtutils/JwtUserDetailsService.java
+++ backend/src/main/java/com/pessom/todo/jwtutils/JwtUserDetailsService.java
@@ -1,5 +1,22 @@
 package com.pessom.todo.jwtutils;
 
-public class JwtUserDetailsService {
-    
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("sobunge".equals(username)) {
+            return new User("sobunge", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        }
+
+    }
+
 }
